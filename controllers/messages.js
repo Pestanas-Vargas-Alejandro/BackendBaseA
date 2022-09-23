9cb4dd3f89d8b20cd6818528d1c11ac02dcf20d3
Alejandro Pestañas Vargas <isc19350316@gmail.com>
2022-09-23 11:00:11 -0500

    first commit

--- controllers/messages.js
+++ controllers/messages.js
@@ -10,4 +10,16 @@ const byeMessages = (req,res)=> {
     res.send('Adios mundo')
 }
 
-module.exports ={rootMessages, hiMessages, byeMessages}
+const postMessage = (req,res)=> {
+    res.send('Mensaje post')
+}
+
+const putMessages = (req,res)=> {
+    res.send('Mensaje put')
+}
+
+const deleteMessage = (req,res)=> {
+    res.send('Mensaje delete')
+}
+
+module.exports ={rootMessages, hiMessages, byeMessages,postMessage,putMessages,deleteMessage}
--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,6 @@
 const {Router} = require ("express")
 const router  = Router()
-const {rootMessages, hiMessages, byeMessages} = require ('../controllers/messages.js')
+const {rootMessages, hiMessages, byeMessages,postMessage,putMessages, deleteMessage} = require ('../controllers/messages.js')
 
 //router.get("",()=> {})
 router.get("/",rootMessages)  //END POINT
@@ -8,5 +8,11 @@ router.get("/",rootMessages)  //END POINT
 router.get("/hi",hiMessages) //END POINT
 
 router.get("/bye",byeMessages) //END POINT
+
+router.post('/',postMessage) //END POINT
+
+router.put('/',putMessages) //END POINT
+
+router.delete('/',deleteMessage) //END POINT
 
 module.exports = router
