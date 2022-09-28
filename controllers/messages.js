ab70085728c8434a367c43270bb3b9913c43ca3c
Alejandro Pestañas Vargas <isc19350316@gmail.com>
2022-09-27 20:20:30 -0500

    mensajes HTTP

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,27 +1,27 @@
 const { request, response } = require("express")
 
 const rootMessages = (req=request,res=response)=> {
-    res.json({msg:'Mensajes'})
+    res.status (404).json({msg:'Mensajes'})
 }
 
 const hiMessages = (req=request,res=response)=> {
-    res.json({hi:'Hola mundo'})
+    res.json({msg:'Hola mundo'})
 }
 
 const byeMessages = (req=request,res=response)=> {
-    res.json({bye:'Adios mundo'})
+    res.json({msg:'Adios mundo'})
 }
 
 const postMessage = (req=request,res=response)=> {
-    res.json({pst:'Mensaje post'})
+    res.json({msg:'Mensaje post'})
 }
 
 const putMessages = (req=request,res=response)=> {
-    res.json({pt:'Mensaje put'})
+    res.json({msg:'Mensaje put'})
 }
 
 const deleteMessage = (req=request,res=response)=> {
-    res.json({del:'Mensaje delete'})
+    res.json({msg:'Mensaje delete'})
 }
 
 module.exports ={rootMessages, hiMessages, byeMessages,postMessage,putMessages,deleteMessage}
