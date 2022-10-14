6b90c4a230efeb2048d71bff1a2b8321b5f5ff8d
Alejandro Pestañas Vargas <isc19350316@gmail.com>
2022-10-14 10:59:07 -0500

    Primer cons BD desde endpoint

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,27 @@
-const { request, response } = require("express");
+const { request, response, json } = require("express")
+const pool = require ("../db/connection")
 
-const getUser = (req=request, res=response) =>{
-    console.log("Funcion getUser")
-    res.json({msg: "Funcion getUser"})
+const getUser = async (req=request, res=response) =>{
+    let conn;
+
+    try {
+        conn = await pool.getConnection()
+        const users = await conn.query("SELECT * FROM Usuarios", (error) => {throw new Error(error)})
+        if (!users){
+            res.status(404).json ({msg: "No se encontraron registros"})
+            return
+        } 
+
+        res.json({users})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({json})
+
+    } finally{
+        if (conn){
+            conn.end()
+        }
+    }
 }
 
 module.exports= {getUser}
