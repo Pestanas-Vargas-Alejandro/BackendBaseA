87055b92e74a6b50ef773ee1c0f66669dc2123bf
Alejandro Pestañas Vargas <isc19350316@gmail.com>
2022-10-18 10:56:05 -0500

    Cons desde front

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -24,4 +24,30 @@ const getUser = async (req=request, res=response) =>{
     }
 }
 
-module.exports= {getUser}
+
+const getUserByID = async (req=request, res=response) =>{
+    const {id}= req.params
+    let conn;
+
+    try {
+        conn = await pool.getConnection()
+        const [user] = await conn.query( `SELECT * FROM Usuarios WHERE ID = ${id}`, (error) => {throw new Error(error)})
+
+        if (!user){
+            res.status(404).json ({msg: `No se encontraron registros con el ID ${id}`})
+            return
+        } 
+
+        res.json({user})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({json})
+
+    } finally{
+        if (conn){
+            conn.end()
+        }
+    }
+}
+
+module.exports= {getUser,getUserByID}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
-const {getUser} = require ("../controllers/usuarios")
+const {getUser, getUserByID} = require ("../controllers/usuarios")
 const router = Router()
 
 router.get("/",getUser)
+router.get("/id/:id/",getUserByID)
 
 module.exports=router
